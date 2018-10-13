bdbc5b9d14fe9b9c43041ba8461fa47304617c44
Shahin <mnajafi0014@gmail.com>
2018-10-13 19:02:08 +0330

    1- add constant for MIME that will use in getType() method in PetProvider inside PetContract class 2- override getType() method in PetProvider class

--- app/src/main/java/com/example/android/pets/data/PetsContract.java
+++ app/src/main/java/com/example/android/pets/data/PetsContract.java
@@ -1,5 +1,6 @@
 package com.example.android.pets.data;
 
+import android.content.ContentResolver;
 import android.net.Uri;
 import android.provider.BaseColumns;
 
@@ -29,6 +30,9 @@ public final class PetsContract {
      */
     public static final String PATH_PETS = "pets";
 
+
+
+
     public static abstract class PetEntry implements BaseColumns{
         //table information in constant
         public static final String TABLE_NAME = "pets";
@@ -45,6 +49,18 @@ public final class PetsContract {
 
         //general content uri that take whole pets table
         public static final Uri CONTENT_URI= Uri.withAppendedPath(BASE_CONTENT_URI,PATH_PETS);
+
+        /**
+         * The MIME type of the {@link #CONTENT_URI} for a list of pets.
+         */
+        public static final String CONTENT_LIST_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_PETS;
+
+        /**
+         * The MIME type of the {@link #CONTENT_URI} for a single pet.
+         */
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_PETS;
     }
 
     public static boolean isValidGender(int gender){
